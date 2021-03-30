b10ebfbc99bc1cdf529f82035f697d08540acf26
Sumant <sumant@Sumants-MacBook-Air.local>
2021-03-30 22:37:46 +0530

    add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import CartItem from "./CartItem";
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <CartItem/>
     </div>
   );
 }
